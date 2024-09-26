ea9ed8590f7f201e2478de8b966aa05aeab6a2ce
Juanse <juansebt.0610@gmail.com>
2024-09-26 15:53:49 -0500

    maquetación de la calculadora - finalizada

--- calculadora.py
+++ calculadora.py
@@ -1,50 +1,88 @@
 import flet as ft
 
+class CalcButton(ft.ElevatedButton):
+    def __init__(self, text=None, expand=1):
+        super().__init__()
+        self.text = text
+        self.expand = expand
+
+class DigitButton(CalcButton):
+    def __init__(self, text, expand=1):
+        CalcButton.__init__(self, text, expand)
+        self.bgcolor = ft.colors.WHITE24
+        self.color = ft.colors.WHITE
+
+class ActionButton(CalcButton):
+    def __init__(self, text):
+        CalcButton.__init__(self, text)
+        self.bgcolor = ft.colors.ORANGE
+        self.color = ft.colors.WHITE
+
+class ExtraActionButton(CalcButton):
+    def __init__(self, text=None, expand=1):
+        CalcButton.__init__(self, text, expand)
+        self.bgcolor = ft.colors.BLUE_GREY_100
+        self.color = ft.colors.BLACK
+
 def main(page: ft.Page):
-    page.title = "CALCULADORA_PY"
-    result = ft.Text(value=0)
+    page.title = "Calculadora_py"
     
+    page.window.width = 370
+    page.window.height = 337
+    
+    result = ft.Text(value="0", color=ft.colors.WHITE, size=20)
+
     page.add(
-        ft.Row(controls=[result]),
-        ft.Row(
-            controls=[
-                ft.ElevatedButton(text="AC"),
-                ft.ElevatedButton(text="+/-"),
-                ft.ElevatedButton(text="%"),
-                ft.ElevatedButton(text="/"),
-            ]
+        ft.Container(
+            width=350,
+            bgcolor=ft.colors.BLACK,
+            border_radius=ft.border_radius.all(20),
+            padding=20,
+            content=ft.Column(
+                controls=[
+                    ft.Row(controls=[result], alignment="end"),
+                    ft.Row(
+                        controls=[
+                            ExtraActionButton(text="Delete", expand=2),
+                            ExtraActionButton(text="%"),
+                            ActionButton(text="/"),
+                        ]
+                    ),
+                    ft.Row(
+                        controls=[
+                            DigitButton(text="7"),
+                            DigitButton(text="8"),
+                            DigitButton(text="9"),
+                            ActionButton(text="*"),
+                        ]
+                    ),
+                    ft.Row(
+                        controls=[
+                            DigitButton(text="4"),
+                            DigitButton(text="5"),
+                            DigitButton(text="6"),
+                            ActionButton(text="-"),
+                        ]
+                    ),
+                    ft.Row(
+                        controls=[
+                            DigitButton(text="1"),
+                            DigitButton(text="2"),
+                            DigitButton(text="3"),
+                            ActionButton(text="+"),
+                        ]
+                    ),
+                    ft.Row(
+                        controls=[
+                            ExtraActionButton(text="C"),
+                            DigitButton(text="0"),
+                            DigitButton(text="."),
+                            ActionButton(text="="),
+                        ]
+                    ),
+                ]
+            )
         ),
-        ft.Row(
-            controls=[
-                ft.ElevatedButton(text="7"),
-                ft.ElevatedButton(text="8"),
-                ft.ElevatedButton(text="9"),
-                ft.ElevatedButton(text="*"),
-            ]
-        ),     
-        ft.Row(
-            controls=[
-                ft.ElevatedButton(text="4"),
-                ft.ElevatedButton(text="5"),
-                ft.ElevatedButton(text="6"),
-                ft.ElevatedButton(text="-"),
-            ]
-        ),     
-        ft.Row(
-            controls=[
-                ft.ElevatedButton(text="1"),
-                ft.ElevatedButton(text="2"),
-                ft.ElevatedButton(text="3"),
-                ft.ElevatedButton(text="+"),
-            ]
-        ),     
-        ft.Row(
-            controls=[
-                ft.ElevatedButton(text="0"),
-                ft.ElevatedButton(text="."),
-                ft.ElevatedButton(text="="),
-            ]
-        ),     
     )
-    
+
 ft.app(target=main)
